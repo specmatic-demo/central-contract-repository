3a669a128ea3ba38ca013a68cc201f791dc12f96
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-02-24 19:27:01 +0530

    add mqtt servers to channels

--- ci.sh
+++ ci.sh
@@ -2,9 +2,9 @@
 
 
 
-java -jar ~/.specmatic/specmatic.jar central-contract-repo-report
+java -jar ~/.specmatic/specmatic-enterprise.jar central-contract-repo-report
 
-java -jar ~/.specmatic/specmatic.jar send-report \
+java -jar ~/.specmatic/specmatic-enterprise.jar send-report \
   --repo-id=$(gh api 'repos/{owner}/{repo}' --jq .id) \
   --repo-name=$(gh repo view --json name -q .name) \
   --repo-url=$(gh repo view --json url --jq .url) \
