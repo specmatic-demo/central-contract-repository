9c955fb02999a3109f9e35a5e02a73c19b78c27c
Ketan Padegaonkar <KetanPadegaonkar@gmail.com>
2026-02-24 11:55:17 +0530

    fix command

--- ci.sh
+++ ci.sh
@@ -1,8 +1,10 @@
 #!/usr/bin/env bash
 
-specmatic central-contract-repo-report
 
-specmatic send-report \
+
+java -jar ~/.specmatic/specmatic.jar central-contract-repo-report
+
+java -jar ~/.specmatic/specmatic.jar send-report \
   --repo-id=$(gh api 'repos/{owner}/{repo}' --jq .id) \
   --repo-name=$(gh repo view --json name -q .name) \
   --repo-url=$(gh repo view --json url --jq .url) \
