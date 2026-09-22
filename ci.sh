af63fb2e76aa4e1ea9feb0f4f7ad9d63e50a7d77
jaydeep <jaydeepk@gmail.com>
2026-09-22 14:30:01 +0530

    fix: set central repo report identity

--- ci.sh
+++ ci.sh
@@ -2,4 +2,12 @@
 
 
 
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+REPO_URL="$(git -C "${SCRIPT_DIR}" config --get remote.origin.url | sed -E 's#^git@github.com:#https://github.com/#; s#\.git$##')"
+REPO_SLUG="${REPO_URL#https://github.com/}"
+export SPECMATIC_REPO_ID="$(gh api "repos/${REPO_SLUG}" --jq .id)"
+export SPECMATIC_REPO_NAME="${REPO_SLUG##*/}"
+export SPECMATIC_REPO_URL="${REPO_URL}"
+export SPECMATIC_BRANCH_NAME="${GITHUB_HEAD_REF:-${GITHUB_REF_NAME:-main}}"
+
 java -jar ~/.specmatic/specmatic-enterprise.jar central-contract-repo-report
